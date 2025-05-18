1dbc0bc2b0a3c02cc8e64468bded7150d28b95c8
Raj Singh <rajubieber80525@gmail.com>
2025-05-18 15:53:47 +0530

    Added simulation for SMS, Email, In-App notifications

--- queues/notificationQueue.js
+++ queues/notificationQueue.js
@@ -5,7 +5,7 @@ const Notification = require('../models/Notification');
 const connection = new Redis({
   host: '127.0.0.1',
   port: 6379,
-  maxRetriesPerRequest: null, // ✅ Required by BullMQ
+  maxRetriesPerRequest: null, 
 });
 
 const notificationQueue = new Queue('notifications', { connection });
@@ -18,7 +18,15 @@ const worker = new Worker(
     if (!notification) return;
 
     try {
-      console.log(`Sending ${notification.type} to User ${notification.userId}: ${notification.message}`);
+        if (notification.type === 'email') {
+            console.log(`Sending EMAIL to user ${notification.userId}: ${notification.message}`);
+          } else if (notification.type === 'sms') {
+            console.log(`Sending SMS to user ${notification.userId}: ${notification.message}`);
+          } else if (notification.type === 'in-app') {
+            console.log(`Storing IN-APP notification for user ${notification.userId}: ${notification.message}`);
+          } else {
+            console.log(`Unknown notification type for user ${notification.userId}: ${notification.message}`);
+          }          
       notification.status = 'sent';
       await notification.save();
     } catch (err) {
